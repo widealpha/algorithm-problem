8236b03275d865e38068b94d615673cc9b9ae09b
kmh <minghaokong77@163.com>
2022-04-09 17:08:49 +0800

    init

---
+++ src/main/java/util/DataUtil.java
@@ -0,0 +1,23 @@
+package util;
+
+import com.google.gson.Gson;
+
+public class DataUtil {
+    static Gson gson = new Gson();
+
+    public static int[] toIntArray(String s) {
+        return gson.fromJson(s, int[].class);
+    }
+
+    public static int[][] toIntTwoArray(String s) {
+        return gson.fromJson(s, int[][].class);
+    }
+
+    public static double[] toDoubleArray(String s) {
+        return gson.fromJson(s, double[].class);
+    }
+
+    public static double[][] toDoubleTwoArray(String s) {
+        return gson.fromJson(s, double[][].class);
+    }
+}
