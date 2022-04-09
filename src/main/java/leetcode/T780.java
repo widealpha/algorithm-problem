0c2426572ff83036a0b294558a854aefbd302e45
kmh <minghaokong77@163.com>
2022-04-09 19:42:56 +0800

    2022.4.9

---
+++ src/main/java/leetcode/T780.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class T780 {
+    public static void main(String[] args) {
+        System.out.println(new T780().reachingPoints(1, 1, 3, 5));
+    }
+
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx > sx && ty > sy && tx != ty) {
+            if (tx > ty) {
+                tx %= ty;
+            } else {
+                ty %= tx;
+            }
+        }
+        if (tx == sx && ty == sy) {
+            return true;
+        } else if (tx == sx && ty > sy) {
+            return (ty - sy) % sx == 0;
+        } else if (tx > sx && ty == sy) {
+            return (tx - sx) % sy == 0;
+        } else {
+            return false;
+        }
+    }
+}
